941eeae6cbdbcdcd24b6d77a774800c4ab0464c5
Leyen <672228275@qq.com>
2025-06-17 14:03:34 +0800

    allow_unsafe_werkzeug

--- data_fetcher/__init__.py
+++ data_fetcher/__init__.py
@@ -51,4 +51,4 @@ quote_ctx.set_on_candlestick(on_candlestick)
 quote_ctx.subscribe_candlesticks(SYMBOL, Period.Min_2)
 
 logger.info("启动成功，当前北京时间：%s" % datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-socketio.run(app, host='0.0.0.0', port=80, debug=True)
+socketio.run(app, host='0.0.0.0', port=80, debug=True, allow_unsafe_werkzeug=True)
